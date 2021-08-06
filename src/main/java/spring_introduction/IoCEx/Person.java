4acb887fa7f530012f65987bcacbbfb67fb91bd2
Egor <sartvas@yandex.ru>
2021-08-06 17:29:56 +0300

    Dependency Injection by setter and constructor

--- src/main/java/spring_introduction/IoCEx/Person.java
+++ src/main/java/spring_introduction/IoCEx/Person.java
@@ -3,7 +3,11 @@ package spring_introduction.IoCEx;
 public class Person {
     Pet pet;
 
-    public Person(Pet pet) {
+    public Person(){
+        System.out.println("Person created");
+    }
+
+    public void setPet(Pet pet) {
         this.pet = pet;
     }
 
