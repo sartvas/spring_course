9750fc76816e1e2b5fcdc3069c8b39fc08c0be29
Egor <sartvas@yandex.ru>
2021-08-10 17:18:03 +0300

    Configuration with annotation:all about @Scope

--- src/main/java/spring_introduction/IoCEx/Hamster.java
+++ src/main/java/spring_introduction/IoCEx/Hamster.java
@@ -1,15 +1,26 @@
 package spring_introduction.IoCEx;
 
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // @Component("hamsterBean") можно не прописывать Component,тогда bean будет hamster, но прописывание - good tone
 //Если class VaforiteSong bean будет vaforiteSong
 //Если class SQLTest bean будет SQLTest
 @Component("hamsterBean")
+@Scope("prototype")
 public class Hamster implements Pet {
+    private String name;
     @Override
     public void say() {
         System.out.println("wt-wt");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/spring_introduction/IoCEx/Test4ConfigAnnotations.java
+++ src/main/java/spring_introduction/IoCEx/Test4ConfigAnnotations.java
@@ -8,13 +8,17 @@ public class Test4ConfigAnnotations {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("aplicationContext4.xml");
 
-//        Hamster hamster = context.getBean("hamsterBean", Hamster.class);
-//        hamster.say();
+        Hamster hamster = context.getBean("hamsterBean", Hamster.class);
+        Hamster myHamster = context.getBean("hamsterBean", Hamster.class);
+        hamster.setName("Nysha");
+        myHamster.setName("Busya");
 
         Person person = context.getBean("personBean", Person.class);
         person.callPet();
         System.out.println(person.getSurname());
         System.out.println(person.getAge());
+
+        System.out.println("We have two hamsters :"+hamster.getName()+" and "+myHamster.getName());
 
         context.close();
     }
